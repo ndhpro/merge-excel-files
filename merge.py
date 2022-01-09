104d8f578d15c617805a0463c08ad2a5e99832f2
ndhpro <ndhpro.psa@gmail.com>
2022-01-09 20:39:28 +0700

    Copy only sum row

--- merge.py
+++ merge.py
@@ -3,65 +3,53 @@ import openpyxl as xl
 from glob import glob
 from copy import copy
 
+try:
+    OUT_FILE = "Tổng hợp.xlsx"
 
-out_name = 'Output.xlsx'
-if os.path.exists(out_name):
-    os.remove(out_name)
+    if os.path.exists(OUT_FILE):
+        os.remove(OUT_FILE)
 
-file_paths = sorted(glob('*.xlsx'))
-out_wb = xl.Workbook()
-out_ws = out_wb.active
-out_r, out_c = 1, 1
-header = False
-for file_path in file_paths:
-    print(file_path)
-    wb = xl.load_workbook(file_path)
+    file_paths = glob("*.xlsx")
+
+    out_wb = xl.Workbook()
+    out_ws = out_wb.active
+
+    is_header = True
+    wb = xl.load_workbook(file_paths[0])
     ws = wb.active
-
-    if not header:
-        is_header = True
-        for rh in range(1, ws.max_row+1):
-            if not is_header:
-                break
-            for ch in range(1, ws.max_column+1):
-                cell = ws.cell(rh, ch)
-                out_ws.cell(out_r, out_c).value = cell.value
-                if cell.has_style:
-                    out_ws.cell(out_r, out_c).fill = copy(cell.fill)
-                    out_ws.cell(out_r, out_c).border = copy(cell.border)
-                    out_ws.cell(out_r, out_c).alignment = copy(cell.alignment)
-                    out_ws.cell(out_r, out_c).font = copy(cell.font)
-                    out_ws.cell(out_r, out_c).protection = copy(
-                        cell.protection)
-                out_c += 1
-                if cell.value == "(27)":
-                    is_header = False
-                    break
-            out_r += 1
-            out_c = 1
-        for mr in list(ws.merged_cells)[1:]:
-            out_ws.merge_cells(str(mr))
-        header = True
-
-    out_r -= 1
-    is_data = False
+    out_r = 1
     for r in range(1, ws.max_row+1):
+        if not is_header:
+            break
         for c in range(1, ws.max_column+1):
             cell = ws.cell(r, c)
-            if is_data:
-                out_ws.cell(out_r, out_c).value = cell.value
-                if cell.has_style:
-                    out_ws.cell(out_r, out_c).fill = copy(cell.fill)
-                    # out_ws.cell(out_r, out_c).border = copy(cell.border)
-                    # out_ws.cell(out_r, out_c).alignment = copy(cell.alignment)
-                out_c += 1
+            out_ws.cell(out_r, c).value = cell.value
+            if cell.has_style:
+                out_ws.cell(out_r, c).fill = copy(cell.fill)
+                out_ws.cell(out_r, c).border = copy(cell.border)
+                out_ws.cell(out_r, c).protection = copy(
+                    cell.protection)
             if cell.value == "(27)":
-                is_data = True
+                is_header = False
                 break
+        out_r += 1
 
-        if is_data:
-            out_r += 1
-            out_c = 1
+    for file_path in file_paths:
+        wb = xl.load_workbook(file_path, data_only=True)
+        ws = wb.active
+        max_value = 0
+        for r in range(1, ws.max_row+1):
+            try:
+                value = int(ws.cell(r, 8).value)
+            except:
+                continue
+            if value > max_value:
+                for c in range(1, ws.max_column+1):
+                    out_ws.cell(out_r, c).value = ws.cell(r, c).value
+                max_value = value
+        out_r += 1
 
-out_wb.save(out_name)
-print('Done!')
+    out_wb.save(OUT_FILE)
+except Exception as e:
+    print(e)
+    input()
